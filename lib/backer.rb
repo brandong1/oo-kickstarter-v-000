538e9b313face5d79dcccc20bc6b52e0d5aaf2fb
Brandon Green <brandon@brandonleeproductions.com>
2018-06-20 01:03:27 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,8 +7,8 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(backed_projects)
-    backed_projects << @backed_projects
+  def back_project(name)
+    @backed_projects << name
   end
 
 end
