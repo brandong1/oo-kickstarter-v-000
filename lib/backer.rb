dd21f1ef298fa1595c8c06391e42843b558b2f92
Brandon Green <brandon@brandonleeproductions.com>
2018-06-20 00:57:11 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,7 +2,7 @@ class Backer
 
   attr_accessor :backed_projects, :name
 
-  def initialize(name)
+  def initialize(name) # takes a name on initialization
     @name = name
     @backed_projects = []
   end
